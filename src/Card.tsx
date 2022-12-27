f3ff6cea99174802bca8fb297ef61704a5f6ed57
Hsienz <55347238+Hsienz@users.noreply.github.com>
2022-12-28 07:56:45 +0800

    completed mobile design

--- src/Card.tsx
+++ src/Card.tsx
@@ -4,7 +4,7 @@ const Card = () => {
 			<div className="font-sans text-white">
 				<h1 className="font-bold text-3xl"> Learn to code by watching others</h1>
 
-				<p className="font-light mt-12">
+				<p className="font-light mt-6">
 					See how experienced developers solve problems in real-time.
 					Watching scripted tutorials is great, but understanding how
 					developers think is invaluable.
