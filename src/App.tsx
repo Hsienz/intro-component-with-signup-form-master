9e4aeaf74141c58b11eb36ce110a75698b3c8943
Hsienz <55347238+Hsienz@users.noreply.github.com>
2022-12-31 01:09:25 +0800

    add background image

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import Card from './Card'
 function App() {
   return (
-    <main className='flex flew-col bg-Red w-full h-fit min-h-full absolute justify-center lg:items-center'>
+    <main className='flex flew-col bg-Red w-full h-fit min-h-full absolute justify-center lg:items-center bg-[url("./assets/bg-intro-mobile.png")] lg:bg-[url("./assets/bg-intro-desktop.png")]'>
       <Card/>
     </main>
   )
